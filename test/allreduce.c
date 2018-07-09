8fc61ec7a99bccb06e23626dbf74a2bdc034dc2c
Salvatore <salvo.dg@gmail.com>
2018-07-09 10:06:03 +0200

    better allreduce test

--- test/allreduce.c
+++ test/allreduce.c
@@ -6,7 +6,7 @@
 
 #define FFCALL(X) { int ret; if (ret=(X)!=FFSUCCESS) { printf("Error: %i\n", ret); exit(-1); } }
 
-#define N 2
+#define N 100
 
 int main(int argc, char * argv[]){
     
@@ -24,36 +24,45 @@ int main(int argc, char * argv[]){
     ffrank(&rank);
     ffsize(&size);
 
-    int32_t to_reduce = 5;
-    int32_t reduced = 0;
+    int32_t * to_reduce = malloc(sizeof(int32_t)*count);;
+    int32_t * reduced = malloc(sizeof(int32_t)*count);;
     
     int failed=0;
     
     ffschedule_h allreduce;
-    ffallreduce(&to_reduce, &reduced, 1, 0, FFSUM, FFINT32, &allreduce);
+    ffallreduce(to_reduce, reduced, count, 0, FFSUM, FFINT32, &allreduce);
 
     MPI_Barrier(MPI_COMM_WORLD); //not needed, just for having nice output
     for (int i=0; i<N && !failed; i++){
-        reduced=0;
+        
+        for (int j=0; j<count; j++){
+            to_reduce[j] = i+j;
+            reduced[j] = 0;
+        }
 
         printf("Rank %i; POST\n", rank);
 
         ffschedule_post(allreduce);
         ffschedule_wait(allreduce);
 
-        printf("Rank %i reduced value: %i\n", rank, reduced);
-        if (reduced != size*to_reduce){
-            printf("FAILED!\n");
-            failed=1;
+        for (int j=0; j<count; j++){
+            if (reduced[j] != (i+j)*size){
+                printf("FAILED!\n");
+                failed=1;
+            }
         }
 
         /* this is ugly... TODO: have internal tags for collectives or allow to change the tag of an operation without changing it */
-        MPI_Barrier(MPI_COMM_WORLD);
+        /* let's rely on the MPI non-overtaking rule for now */
+        //MPI_Barrier(MPI_COMM_WORLD);
     }
 
     ffschedule_delete(allreduce);
 
     fffinalize();
+    
+    free(reduced);
+    free(to_reduce);
 
     if (!failed){
         printf("PASSED!\n");
